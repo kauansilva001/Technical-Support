d29e57bfbc777074c4b17165c3b2f93a2590f129
unknown <you@example.com>
2023-10-02 17:15:56 -0300

    login

---
+++ loginadm.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/support-icon.ico" type="image/x-icon">
+    <link rel="stylesheet" href="style/loginadm.css">
+    <title>Login Adiministrador</title>
+</head>
+<body>
+<form>
+<table>
+    <tr><td><label>Email:</label></td></tr>
+    <tr><td><input type="email"></td>
+    <tr><td><label>Senha:</label></td></tr>
+    <tr><td><input type="password"></td>
+</table>
+<button>Enviar</button>
+<a href="recuperarSenha.php">Esqueci minha senha</a>
+    </form>
+</body>
+</html>
