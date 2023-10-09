e1e8a3f3da698eef87da0b4063e7ed0f79c67506
sabrinaolivei <sabrina.oliveira200@etec.sp.gov.br>
2023-10-09 14:26:51 -0300

    estilização do login do adm

--- loginadm.php
+++ loginadm.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -18,5 +18,32 @@
 <button>Enviar</button>
 <a href="recuperarSenha.php">Esqueci minha senha</a>
     </form>
+</body>
+</html> --> 
+
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="icons/support-icon.ico" type="image/x-icon">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link rel="stylesheet" href="style/style.css">
+    <title>Sistema de Chamado</title>
+</head>
+<body>
+    <main>
+        <form action="controler.php" method="POST">
+            <table>
+            <tr><td><label>Email institucional:</label></td></tr>
+            <tr><td><div class="division-grid"><div class="division-icon"><i class='bx bx-envelope'></i></div><input class="input-style" type="email"></td></div> <!--Lembrar de colocar o nome para o uso do PHP--> </tr>
+
+            <tr><td><label>Senha:</label></td></tr>
+            <tr><td><div class="division-grid"><div class="division-icon"><i class='bx bx-lock-alt'></i></div><input class="input-style" type="text"></div></td></tr>
+
+            </table>
+            <td><tr><input class="input-button" type="su  mit"></tr></td>        </form>
+    </main>
 </body>
 </html>
