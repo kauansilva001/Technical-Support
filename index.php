b672625121081b3afef95b28a950dfc3c5b0f37c
kauansilva001 <kauan.silva146@etec.sp.gov.br>
2023-10-23 18:37:20 -0300

    Modificações para laptop

--- index.php
+++ index.php
@@ -50,7 +50,7 @@
             </div>
         </form>
         <div class="space-image">
-                <img src="icons/IMAGE-INDEX.svg" alt="Ilustração na qual a personagem usa um computador" width="90%">
+                <img src="icons/IMAGE-INDEX.svg" alt="Ilustração na qual a personagem usa um computador" width="95%">
         </div>
     </div>
 </main>
