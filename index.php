80c1e02a09065a75180d5a2d14f96605dd576448
kauansilva001 <kauan.silva146@etec.sp.gov.br>
2023-10-26 15:09:11 -0300

    Arrumando as trapalhadas da Sabrina

--- index.php
+++ index.php
@@ -55,10 +55,5 @@
        
     </div>
 </main>
-<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6CwfuxIqcltXDGjfZsMd9A?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
-
-    <div>
-    <img src="santineli.jpg" alt="">        
-</div>
 </body>
 </html>
