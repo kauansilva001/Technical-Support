a8059bcad914848116e753c683848c4d629a0198
kauansilva001 <kauan.silva146@etec.sp.gov.br>
2023-10-26 12:17:04 -0300

    Adicionando graça e beleza no visual

--- index.php
+++ index.php
@@ -55,5 +55,6 @@
        
     </div>
 </main>
+<iframe style="border-radius:12px" src="https://open.spotify.com/embed/artist/6CwfuxIqcltXDGjfZsMd9A?utm_source=generator" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
 </body>
 </html>
