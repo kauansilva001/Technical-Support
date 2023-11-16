d8eeadf948af2f9b665b3c6fcf20aeb4b2dcd3ec
kauansilva001 <kauan.silva146@etec.sp.gov.br>
2023-11-16 14:30:55 -0300

    Fazendo cabeçalho das tabelas

--- listarchamados.php
+++ listarchamados.php
@@ -5,94 +5,105 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="shortcut icon" href="IMAGES-ICONS/support-icon.ico" type="image/x-icon">
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
   <title>Sistema de chamados</title>
 </head>
 
 <body>
+  <?php
 
-  <table class=" table table-danger table-stripped">
+  $cod_chamados = filter_input(INPUT_POST, "cod_chamados");
+  $email = filter_input(INPUT_POST, "email");
+  $desc_problema = filter_input(INPUT_POST, "desc_problema");
+  $lugar = filter_input(INPUT_POST, "lugar");
+  $tipo_problema = filter_input(INPUT_POST, "tipo_problema", );
+  $tipo_user = filter_input(INPUT_POST, "tipo_user", );
+  $data_hora = filter_input(INPUT_POST, "data_hora", );
+
+
+  include("PHP/chamadosDAO.php");
+  include("PHP/chamados.php");
+
+  $chamados = new chamados();
+
+  $chamados->setCod_chamados($cod_chamados);
+  $chamados->setEmail($email);
+  $chamados->setDesc_problema($desc_problema);
+  $chamados->setLugar($lugar);
+  $chamados->setTipo_problema($tipo_problema);
+  $chamados->setTipo_user($tipo_user);
+  $chamados->setData_hora($data_hora);
+
+  $chamadosDAO = new chamadosDAO();
+  $chamadosDAO->consultarChamados();
+
+  $conferirChamados = $chamadosDAO->consultarChamados();
+  $resultadoConsulta = $chamadosDAO->consultarChamadosResolvidos();
+
+  if ($conferirChamados == true) {
+
+    foreach ($conferirChamados as $consult) {
+      echo $consult['cod_chamados'] . "<br>";
+      echo $consult['email'] . "<br>";
+      echo $consult['desc_problema'] . "<br>";
+      echo $consult['lugar'] . "<br>";
+      echo $consult['tipo_problema'] . "<br>";
+      echo $consult['tipo_user'] . "<br>";
+      echo $consult['data_hora'] . "<hr>";
+    }
+  } else {
+    echo "Não há chamados" . "<br>";
+  }
+
+  if ($resultadoConsulta !== false) {
+    foreach ($resultadoConsulta as $consult) {
+      echo $consult['cod_chamados'] . "<br>";
+      echo $consult['email'] . "<br>";
+      echo $consult['desc_problema'] . "<br>";
+      echo $consult['lugar'] . "<br>";
+      echo $consult['tipo_problema'] . "<br>";
+      echo $consult['tipo_user'] . "<br>";
+      echo $consult['data_hora'] . "<hr>";
+    }
+  } else {
+    if ($conferirChamados == true) {
+      echo "Não há chamados resolvidos";
+    }
+  }
+
+  ?>
+
+  <table class="table table-danger table-stripped">
     <thead>
       <tr>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
-        <th scope="col"></th>
+        <th scope="col">Código do chamado</th>
+        <th scope="col">Email Institucional</th>
+        <th scope="col">Descrição do problema</th>
+        <th scope="col">Local do problema</th>
+        <th scope="col">Tipo de problema</th>
+        <th scope="col">Tipo de usuário</th>
+        <th scope="col">Data e Hora</th>
       </tr>
     </thead>
+    <tr>
+      <td></td>
+    </tr>
+  </table>
+
+  <table class="table table-success">
+
   </table>
 
 
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
+    crossorigin="anonymous"></script>
 </body>
 
 
 
 
-<?php
 
-$cod_chamados = filter_input(INPUT_POST, "cod_chamados");
-$email = filter_input(INPUT_POST, "email");
-$desc_problema = filter_input(INPUT_POST, "desc_problema");
-$lugar = filter_input(INPUT_POST, "lugar");
-$tipo_problema = filter_input(INPUT_POST, "tipo_problema",);
-$tipo_user = filter_input(INPUT_POST, "tipo_user",);
-$data_hora = filter_input(INPUT_POST, "data_hora",);
-
-
-include("PHP/chamadosDAO.php");
-include("PHP/chamados.php");
-
-$chamados = new chamados();
-
-$chamados->setCod_chamados($cod_chamados);
-$chamados->setEmail($email);
-$chamados->setDesc_problema($desc_problema);
-$chamados->setLugar($lugar);
-$chamados->setTipo_problema($tipo_problema);
-$chamados->setTipo_user($tipo_user);
-$chamados->setData_hora($data_hora);
-
-$chamadosDAO = new chamadosDAO();
-$chamadosDAO->consultarChamados();
-
-$conferirChamados = $chamadosDAO->consultarChamados();
-$resultadoConsulta = $chamadosDAO->consultarChamadosResolvidos();
-
-if ($conferirChamados == true) {
-
-  foreach ($conferirChamados as $consult) {
-    echo $consult['cod_chamados'] . "<br>";
-    echo $consult['email'] . "<br>";
-    echo $consult['desc_problema'] . "<br>";
-    echo $consult['lugar'] . "<br>";
-    echo $consult['tipo_problema'] . "<br>";
-    echo $consult['tipo_user'] . "<br>";
-    echo $consult['data_hora'] . "<hr>";
-  }
-} else {
-  echo "Não há chamados" . "<br>";
-}
-
-if ($resultadoConsulta !== false) {
-  foreach ($resultadoConsulta as $consult) {
-    echo $consult['cod_chamados'] . "<br>";
-    echo $consult['email'] . "<br>";
-    echo $consult['desc_problema'] . "<br>";
-    echo $consult['lugar'] . "<br>";
-    echo $consult['tipo_problema'] . "<br>";
-    echo $consult['tipo_user'] . "<br>";
-    echo $consult['data_hora'] . "<hr>";
-  }
-} else {
-  if ($conferirChamados == true) {
-    echo "Não há chamados resolvidos";
-  }
-}
-
-?>
 
 </html>
